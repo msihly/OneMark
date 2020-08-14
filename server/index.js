9e332ad62f78928d2f5295ac397d826a22b249f2
msihly <43391570+msihly@users.noreply.github.com>
2020-08-14 15:40:36 -0400

    Adjusted folder structure and naming schema

--- server/index.js
+++ server/index.js
@@ -27,13 +27,13 @@ try {
     app.use(session(sessionSetup));
     app.use(multer({ limits: { fieldSize: 25 * 1024 * 1024 } }).any());
     app.use(express.json());
-    app.use(express.static(path.join(__dirname, "../build")));
+    app.use(express.static(path.join(__dirname, "../client/build")));
 
     const normalizedPath = path.join(__dirname, "routes");
     require("fs").readdirSync(normalizedPath).forEach(file => require("./routes/" + file));
 
     app.get("*", (req, res) => {
-        return res.sendFile(path.join(__dirname, "../build", "index.html"));
+        return res.sendFile(path.join(__dirname, "../client/build", "index.html"));
     });
 
     app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
